a670a643798ded033d67f3014f9f283c1bfab297
MKortmann <companymvms@gmail.com>
2019-01-13 20:40:49 +0100

    style: add some effects as background card colors

--- javascript/app.js
+++ javascript/app.js
@@ -1,58 +1,64 @@
 "use strict";
 
 /*Board Construction Initialization*/
-var arrayIcons = ["3d_rotation","fingerprint","delete","delete","bug_report","extension","extension","android",
-                  "card_giftcard","fingerprint","face","face","card_giftcard","3d_rotation","android","bug_report"];
-var arraySpan = ["#span-9","#span-10","#span-11","#span-4","#span-5", "#span-13","#span-7","#span-14",
-                  "#span-1","#span-2","#span-3","#span-12","#span-6", "#span-8","#span-15","#span-16"];
-/*
-var arrayIcons = ["3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation",
+let arrayIcons = ["3d_rotation","fingerprint","delete","delete",
+                  "bug_report","extension","extension","android",
+                  "card_giftcard","fingerprint","face","face",
+                  "card_giftcard","3d_rotation","android","bug_report"];
+
+let arraySpan = ["#span-9","#span-10","#span-11","#span-4",
+                 "#span-5","#span-13","#span-7","#span-14",
+                 "#span-1","#span-2","#span-3","#span-12",
+                 "#span-6", "#span-8","#span-15","#span-16"];
+/*Array below is only to test*/
+/*arrayIcons = ["3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation",
                   "3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation","3d_rotation"];*/
 
-var randomNumber = 0;
-var arrayIconsRandom = new Array(16);
+let randomNumber = 0;
+let arrayIconsRandom = new Array(16);
+
 /*Creating random numbers using this forLoop and Math function*/
 for (let iLoop=arrayIcons.length-1; iLoop>= 0; iLoop--) {
   randomNumber = Math.floor(Math.random()*iLoop);
   arrayIconsRandom[iLoop] = arrayIcons[randomNumber];
-  arrayIcons.splice(randomNumber,1); /*to make sure it will keep the same elements-so remove the used element*/
-}
+  arrayIcons.splice(randomNumber,1);/*to make sure it will keep the same elements-so remove the used element*/
+};
+
 /*Game Grid Initialization*/
 for (let iLoop = 15; iLoop >= 0; iLoop--) {
   document.querySelector(arraySpan[iLoop]).textContent = arrayIconsRandom[iLoop];
   arrayIcons.splice(iLoop,1);
   arraySpan.splice(iLoop,1);
-}
+};
 
 /*Game functions variables*/
-var flipIndex = 0; /*track the number of flip cards*/
-var arrayIconsFlipped = ["none", "none-1"]; /*store the name of icons to compare*/
-var arraySpanIdFlipped = new Array(2); /*store the id of span elements flipped*/
-var arrayDivIdFlipped = new Array(2); /*store the div of elements flipped*/
-var flipCorrectIndex = 0; /*to track the end of the game*/
-var flipMissIndex = 0; /*to track misses*/
-var start_time = Date.now(); /*game start with the first click*/
-var runTime = 0;
-var runMinutes = 0;
+let flipIndex = 0; /*track the number of flip cards*/
+let arrayIconsFlipped = ["none", "none-1"];/*store the name of icons to compare*/
+let arraySpanIdFlipped = new Array(2); /*store the id of span elements flipped*/
+let arrayDivIdFlipped = new Array(2); /*store the div of elements flipped*/
+let flipCorrectIndex = 0; /*to track the end of the game*/
+let flipMissIndex = 0; /*to track misses*/
+let startTime = Date.now(); /*game start with the first click*/
+let runTime = 0;
 
 /*Timer Funcion*/
 function runTimer(){
-    runTime = Date.now() - start_time ;
-    var timeElapsedMillisec = Math.floor(runTime/1000);
-    document.querySelector("#spanTimerM").textContent = Math.floor(timeElapsedMillisec / 60);
-    document.querySelector("#spanTimerS").textContent =  timeElapsedMillisec - (Math.floor(timeElapsedMillisec / 60)*60 );
-};
+    runTime = Date.now() - startTime ;
+    let timeElapsedMillisec = Math.floor(runTime/1000);
+    document.querySelector("#span-timer-m").textContent = Math.floor(timeElapsedMillisec / 60);
+    document.querySelector("#span-timer-s").textContent =  timeElapsedMillisec - (Math.floor(timeElapsedMillisec / 60)*60 );
+}
 
 /*Effect change of Element*/
-function effect() {
-  document.getElementById(arrayDivIdFlipped[0]).classList.add("effect");
-  document.getElementById(arrayDivIdFlipped[1]).classList.add("effect");
+function effectError() {
+  document.getElementById(arrayDivIdFlipped[0]).classList.add("effect-error");
+  document.getElementById(arrayDivIdFlipped[1]).classList.add("effect-error");
 }
 
 /*Effect change of Element*/
 function effectCorrect() {
-  document.getElementById(arrayDivIdFlipped[0]).classList.add("effectCorrect");
-  document.getElementById(arrayDivIdFlipped[1]).classList.add("effectCorrect");
+  document.getElementById(arrayDivIdFlipped[0]).classList.add("effect-correct");
+  document.getElementById(arrayDivIdFlipped[1]).classList.add("effect-correct");
 }
 
 /*reset to restart the round NOT THE GAME*/
@@ -69,9 +75,9 @@ function reset() {
 /*Cards do not match*/
 function redoFlip () {
   flipMissIndex++;
-  document.querySelector("#spanMiss").textContent = flipMissIndex;
-  document.getElementById(arrayDivIdFlipped[0]).classList.remove("effect");
-  document.getElementById(arrayDivIdFlipped[1]).classList.remove("effect");
+  document.querySelector("#span-miss").textContent = flipMissIndex;
+  document.getElementById(arrayDivIdFlipped[0]).classList.remove("effect-error");
+  document.getElementById(arrayDivIdFlipped[1]).classList.remove("effect-error");
   document.getElementById(arraySpanIdFlipped[0]).classList.toggle("material-icons");
   document.getElementById(arraySpanIdFlipped[0]).classList.toggle("hide");
   document.getElementById(arraySpanIdFlipped[1]).classList.toggle("material-icons");
@@ -81,7 +87,7 @@ function redoFlip () {
 /*Cards match*/
 function matchCards () {
   flipCorrectIndex++;
-  document.querySelector("#spanHits").textContent = flipCorrectIndex;
+  document.querySelector("#span-hits").textContent = flipCorrectIndex;
   effectCorrect();
   reset();
 }
@@ -89,25 +95,20 @@ function matchCards () {
 function showCards() {
   document.getElementById(arraySpanIdFlipped[flipIndex]).classList.toggle("material-icons"); /*display element*/
   document.getElementById(arraySpanIdFlipped[flipIndex]).classList.toggle("hide"); /*display element*/
-  document.getElementById(arrayDivIdFlipped[flipIndex]).classList.toggle("effectRotate");
-
+  document.getElementById(arrayDivIdFlipped[flipIndex]).classList.toggle("effect-rotate");
   flipIndex++;
 
-  if (flipIndex == 2)
-  {
+  if (flipIndex == 2) {
 
-    if(arrayIconsFlipped[0] === arrayIconsFlipped[1])
-    {
+    if( (arrayIconsFlipped[0] === arrayIconsFlipped[1]) && (arrayDivIdFlipped[0] != arrayDivIdFlipped[1]) ) { /*avoid to count at same card*/
       matchCards ();
-      if (flipCorrectIndex == 8)
-      {
-        setTimeout(location.reload(),3000);
+      if (flipCorrectIndex == 8) {
+        setTimeout(location.reload,1000);
         /*location.reload(); /*restart the game*/
       }
-    } else
-    {
+    } else {
       document.querySelector("#grid-container").removeEventListener("click", runGame, true); /*prevent the user from selecting the same card twice*/
-      effect();
+      effectError();
       setTimeout(redoFlip,1000);
     }
   }
@@ -121,11 +122,11 @@ function runGame (evt) {
   arrayIconsFlipped[flipIndex] = evt.target.textContent; /*get the name of element flipped*/
   arraySpanIdFlipped[flipIndex] = evt.target.children[0].id; /*get the span of element flipped*/
   arrayDivIdFlipped[flipIndex] = evt.path[0].id; /*get the div of element flipped*/
-  document.getElementById(arrayDivIdFlipped[flipIndex]).classList.toggle("effectRotate");
+  document.getElementById(arrayDivIdFlipped[flipIndex]).classList.toggle("effect-rotate");
   setTimeout(showCards,500);
 }
 
-/*button functionality*/
+/*button refresh page*/
 document.querySelector("#buttonRestart").addEventListener("click", function(){
   location.reload()
 });
